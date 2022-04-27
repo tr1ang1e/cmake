6db374766f77bd4d86da1d204d1404952097e984
tr1ang1e <tr1ang1e.goog1e@gmail.com>
2022-04-27 13:59:33 +0300

    3.0.0.0

--- tutorial/include/cmake_config.h
+++ tutorial/include/cmake_config.h
@@ -1,10 +1,10 @@
 // get project version info from CMakeLists.txt = PROJECT(...)
 // to get version = @<PROJECTNAME>_VERSION_<TYPE>@
-#define VERSION_MAJOR 1
+#define VERSION_MAJOR 2
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_TWEAK 1
+#define VERSION_TWEAK 0
 
 // get options declared by OPTION() and transform to C/C++ define macros
 /* #undef  */
-/* #undef mv_LOGGING_on */
+#define mv_LOGGING_on
--- tutorial/include/proj_version.h
+++
@@ -1,6 +0,0 @@
-// get project version info from CMakeLists.txt = PROJECT(...)
-// to get version = 
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 0
-#define VERSION_TWEAK 1
