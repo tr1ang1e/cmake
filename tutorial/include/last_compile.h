b2a0a3845a6daec25f662ccd1123cabedc6a51d4
tr1ang1e <tr1ang1e.goog1e@gmail.com>
2022-04-30 23:19:24 +0300

    9.0.0.0

--- tutorial/include/last_compile.h
+++ tutorial/include/last_compile.h
@@ -1,6 +1,6 @@
 #ifndef LAST_COMPILE_H 
 #define LAST_COMPILE_H 
 
-const char* _LC_ = "Last compile time 2022-04-30 17:15:17";  // last compile time 
+const char* _LC_ = "Last compile time 2022-04-30 23:19:13";  // last compile time 
 
 #endif // LAST_COMPILE_H 
