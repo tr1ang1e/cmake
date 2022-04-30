2204f722db8e58633051edcacfaaff7194374668
tr1ang1e <tr1ang1e.goog1e@gmail.com>
2022-04-30 17:16:09 +0300

    7.0.0.0

--- tutorial/include/cmake_config.h
+++ tutorial/include/cmake_config.h
@@ -1,6 +1,6 @@
 // get project version info from CMakeLists.txt = PROJECT(...)
 // to get version = @<PROJECTNAME>_VERSION_<TYPE>@
-#define VERSION_MAJOR 5
+#define VERSION_MAJOR 7
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 #define VERSION_TWEAK 0
